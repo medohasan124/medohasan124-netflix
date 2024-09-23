8947e3e90cb633e2214369abca1265b852f44131
medo <medo@gmail.com>
2024-09-24 02:10:09 +0300

    v.0.1 start

---
+++ nitflex/routes/admin/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::middleware([
+    'localeSessionRedirect',
+    'localizationRedirect',
+    'localeViewPath',
+    // 'auth',
+    // 'role:SuperAdmin|Admin'
+])
+->prefix(LaravelLocalization::setLocale())
+->group(function(){
+    Route::name('admin.')
+            ->prefix('admin')
+            ->group(function(){
+                Route::get('dashboard', function () {
+                    return view('admin.dashboard.index');
+                });
+            });
+});
+
+
+
+
+
+
+    Auth::routes();
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
