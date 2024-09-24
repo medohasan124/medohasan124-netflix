273d389dec2f3ba04e36f48bf351f68a5c474a28
medo <medo@gmail.com>
2024-09-24 23:57:33 +0300

    v.0.2 add tailwind and dashbaord

---
+++ nitflex/resources/views/components/layouts/index.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html x-data="{ darkMode: localStorage.getItem('dark') === 'true'}"
+			x-init="$watch('darkMode', val => localStorage.setItem('dark', val))"
+			x-bind:class="{ 'dark': darkMode }">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> {{ $title }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class='bg-white dark:bg-gray-900'>
+
+    <x-layouts.sidebar class="min-w-fit flex-grow-0 flex-shrink-0 hidden md:block"/>
+
+    <div class='p-4 sm:ml-64'>
+        <div class="block sm:absolute top-5 right-8 order-1">
+            <x-dark-mode-toggle size="4" />
+        </div>
+
+
+            {{ $slot }}
+            <x-layouts.footer />
+    </div>
+
+</body>
+
+</html>
--- nitflex/tailwind.config.js
+++ nitflex/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-      ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
+    darkMode: 'class',
+  }
